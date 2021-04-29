0b94522d655bb7e987c3a15842f227d2d776cc14
YK911 <jaroslavkositsa@gmail.com>
2021-04-29 20:35:43 +0300

    gsap practice

---
+++ script.js
@@ -0,0 +1,16 @@
+const tl = gsap.timeline();
+
+tl.fromTo('#second', {x: '-100%', y: '+100%'}, {y: 0})
+tl.fromTo('#third', {x: '-100%'}, {x: '-200%'})
+tl.fromTo('#fourth', {x: '-400%'}, {x: '-300%'})
+
+const main = document.querySelector('.main')
+
+ScrollTrigger.create({
+    animation: tl,
+    trigger: '.page',
+    start: 'top top',
+    end: () => main.offsetWidth / 2,
+    scrub: true,
+    pin: true
+});
